b3304e2a459d27212698853b67f76fcc34fd0993
Wasgo097 <patrykpakula321@gmail.com>
2021-12-19 15:31:29 +0100

    Add migrations

--- Model/Migrations/20211219142753_AddCvAndDesc.Designer.cs
+++ Model/Migrations/20211219142753_AddCvAndDesc.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Model;
 
 namespace Model.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20211219142753_AddCvAndDesc")]
+    partial class AddCvAndDesc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
